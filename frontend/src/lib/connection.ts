db9b9996a93a2c7978604059b8ff10d737c0e06a
Krishna Garg <krishnagaarg@gmail.com>
2025-11-20 13:01:35 +0000

    fixed some errors

--- frontend/src/lib/connection.ts
+++ frontend/src/lib/connection.ts
@@ -1,4 +1,5 @@
 import { clusterApiUrl } from "@solana/web3.js";
 
 export const NETWORK = "devnet";
-export const RPC_ENDPOINT = clusterApiUrl(NETWORK);
+export const RPC_ENDPOINT =
+  import.meta.env.VITE_SOLANA_RPC || clusterApiUrl(NETWORK);
